f9e7a93017051960c32d1c4c9340fb378348b7d7
ageekwall <agrawalsoumya94@gmail.com>
2020-03-20 06:24:27 -0500

    bugfix

--- server-client/frontend.js
+++ server-client/frontend.js
@@ -18,11 +18,10 @@ $(function () {
     // open connection
     var connection = new WebSocket('ws://127.0.0.1:5000');
 
-    // connection.onopen = function () {
-    //     // first we want users to enter their names
-    //     input.removeAttr('disabled');
-    //     status.text('');
-    // };
+    connection.onopen = function () {
+        input.removeAttr('disabled');
+        status.text('');
+    };
 
     connection.onerror = function (error) {
         // just in there were some problems with conenction...
